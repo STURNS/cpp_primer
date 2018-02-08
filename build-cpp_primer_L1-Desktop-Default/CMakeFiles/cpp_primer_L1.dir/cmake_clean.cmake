222bc8061ab91a35a142e2c8e233912d1637e8e3
STURNS <linyuyu27906@gmail.com>
2018-02-07 17:31:25 -0800

    L1

---
+++ build-cpp_primer_L1-Desktop-Default/CMakeFiles/cpp_primer_L1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_primer_L1.dir/main.cpp.o"
+  "cpp_primer_L1.pdb"
+  "cpp_primer_L1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_primer_L1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
